9d812b35ed78ed30213f21fdf86b64ed9808f8b5
= <=>
2019-03-14 21:38:37 +0200

    [UPDATE] specification du controller dans le fichier splash.fxml

--- LibraryLaus/src/librarylaus/SplashController.java
+++ LibraryLaus/src/librarylaus/SplashController.java
@@ -23,7 +23,7 @@ public class SplashController implements Initializable{
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
     
 }
