d7d1b711b910a54a0d12feb029275f3dc33532a1
= <=>
2019-03-16 21:16:58 +0200

    [ADD] ajout du package image

--- LibraryLaus/src/librarylaus/SplashController.java
+++ LibraryLaus/src/librarylaus/SplashController.java
@@ -9,6 +9,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.image.ImageView;
 
 
@@ -20,10 +21,16 @@ public class SplashController implements Initializable{
     
     @FXML
     private ImageView imViewScreen;
+    @FXML 
+    private ProgressBar progressbar;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        
+//        imViewScreen.setImage('images.jpeg');
+//        for(int i=0; i<=4; i++){
+//            progressbar.setProgress(i);
+//        }
+          
     }
     
 }
