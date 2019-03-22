aa454b6f8a0069db8dde9b3a8c6833a1ba943a2a
= <=>
2019-03-22 10:35:50 +0200

    [UPDATE] update singleton database connect

--- LibraryLaus/src/librarylaus/database/ConnexionDB.java
+++ LibraryLaus/src/librarylaus/database/ConnexionDB.java
@@ -13,48 +13,15 @@ import librarylaus.infoConnect.Infos;
 /**
  * @author gaelmmg
  */
-
 // creation de la classe Singleton pour la connexion a 
 //                la base des donnees
 
 
 public class ConnexionDB {
     
-    private static Connection connect = null;
-    private static ConnexionDB conDb = null;
-    
-    private Infos info;
-    
-    
-    private ConnexionDB() throws SQLException{
-        this.initialise();
-    }
-    
-    private void initialise(){
-        
-        String url = "jdbc:mysql://localhost:/Laus";
-        String user = "root";
-        String pwd = "";
-        
-        this.info = new Infos();
-        
-        this.info.setUrl(url);
-        this.info.setUser(user);
-        this.info.setPwd(pwd);
-    }
-    
-    public static ConnexionDB getInstance() throws SQLException { 
-        
-        if(ConnexionDB.conDb == null){ ConnexionDB.conDb = new ConnexionDB(); }
-        return ConnexionDB.conDb; 
-    }
-    
-    public Connection getConnexion() throws SQLException{ 
-        
-        if(ConnexionDB.connect == null){ ConnexionDB.connect = DriverManager.getConnection(this.info.getUrl(), 
-                                                                                           this.info.getUser(),
-                                                                                           this.info.getPwd()); }
-        System.out.println("connexion reussie");
-        return this.connect; }
+            
+    private String url      = "jdbc:mysql://localhost/Laus";
+    private String user     = "root";
+    private String password = "";
     
 }
