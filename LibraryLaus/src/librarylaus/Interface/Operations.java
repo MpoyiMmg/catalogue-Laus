94369bf6b9b96472cc105626dd31c31ee0227ca0
= <=>
2019-03-24 21:58:19 +0200

    [ADD] create abstract method delete

--- LibraryLaus/src/librarylaus/Interface/Operations.java
+++ LibraryLaus/src/librarylaus/Interface/Operations.java
@@ -13,4 +13,6 @@ public interface Operations {
     
     public abstract void insert(Object obj);
     public abstract void update(Object obj);
+    public abstract void delete(Object obj);
+    
 }
